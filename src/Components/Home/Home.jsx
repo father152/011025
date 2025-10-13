ead660bc87db877d5ebb9790a5796e34dbc72a3e
Your Name <tsimbalenko.vv@gmail.com>
2025-10-13 09:37:02 +0300

    455

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./home.css";
-
+import { Link } from "react-router-dom";
 
 
 
@@ -10,10 +9,33 @@ export default function Home() {
     <div className="home">
             <h1>Home</h1>
       <div className="home-container">
+     
+      <div className="left-side">
+      <h4>CONTENT</h4>     
+      <div className="left-container">
+              
+              <ul className="left-title">
+                <li>
+                  <Link to="/region">Region building</Link>
+                </li>
+                <li>
+                  <Link to="/house">House</Link>
+                </li>
+                <li>
+                  <Link to="/floor">Floor </Link>
+                </li>
+                <li>
+                  <Link to="/flat">Flat</Link>
+                </li>
+              </ul>
       
+            </div>
       </div>
-
+      <div className="centre-side"></div>
       
+ 
+      
+    </div>
     </div>
   );
 }
