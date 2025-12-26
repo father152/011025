001f084f9cb50220a0e00f9fac76a7e488cbad3a
Your Name <tsimbalenko.vv@gmail.com>
2025-12-26 11:23:12 +0200

    78

--- src/Components/Header/Projects.jsx
+++ src/Components/Header/Projects.jsx
@@ -13,6 +13,26 @@ export default function Projects() {
       <div className="projects-container">
 <div className="girl">
   <img src={girl} alt=""></img>
+  <p>
+                 nec metus bibendum
+                egestas. Iaculis massa nisl malesuada lacinia integer nunc
+                posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
+                Ad litora torquent per conubia nostra inceptos himenaeos. Lorem
+                ipsum dolor sit amet consectetur adipiscing elit. Quisque
+                faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+                mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+                urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+                egestas. Iaculis massa nisl malesuada lacinia integer nunc
+                posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
+                Ad litora torquent per conubia nostra inceptos himenaeos. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem
+                ipsum dolor sit amet consectetur adipiscing elit. Quisque
+                faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+                mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+                urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+                egestas. Iaculis massa nisl malesuada lacinia integer nunc
+                posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
+                Ad litora torquent per conubia nostra inceptos himenaeos.
+              </p>
 </div>
 
        
