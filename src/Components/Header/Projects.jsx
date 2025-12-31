90ec0d6940150078582cefaef370452127ed1cbe
Your Name <tsimbalenko.vv@gmail.com>
2025-12-31 08:21:22 +0200

    56

--- src/Components/Header/Projects.jsx
+++ src/Components/Header/Projects.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import home from "../Home/home.png";
 import "./projects.css";
-
+import girl from "../Home/b82e171512b51e24a23bb9d0cebfe8ca.jpg"
 export default function Projects() {
       return (
     <div className="projects">
       <h1>Projects</h1>
       <div className="projects-container">
-      <h2>Future Projects Coming Soon...</h2>
+      <h2>
+        <img src={girl} alt=""></img>Future Projects 
+        Coming Soon...</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
-      <h2>Future Projects Coming Soon...</h2>
+      <h2>Future Projects 
+        <img src={girl} alt=""></img>
+        Coming Soon...</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
       </div>
 
